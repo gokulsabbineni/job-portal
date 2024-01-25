aaf8d32915650a1bdca05406e1d4dab227cdd1a6
udayteja260 <udayteja260@gmail.com>
2024-01-24 19:22:06 -0600

    User Management

---
+++ api/userManagement/routes.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func RegisterRoutes(router *mux.Router, db *mongo.Client) {
+
+	router.HandleFunc("/users", createUser(db))
+	router.HandleFunc("/users/list/{id}", getUser(db)).Methods(http.MethodGet)
+	router.HandleFunc("/users/update/{id}", func(w http.ResponseWriter, r *http.Request) { updateUser(db, w, r) }).Methods(http.MethodPut)
+
+}
---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	api "Project/api/userManagement"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// Connect to MongoDB database
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Check the MongoDB connection
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+	router := mux.NewRouter()
+	api.RegisterRoutes(router, client)
+
+	log.Println("Server starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
